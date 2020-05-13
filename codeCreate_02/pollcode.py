ce6b0b8c949428a47a3a3ee4a6595c5f8ab8a767
wanglonglong <wanglonglong@teddymobile.cn>
2020-05-13 14:34:57 +0800

    fix

--- codeCreate_02/pollcode.py
+++ codeCreate_02/pollcode.py
@@ -58,7 +58,7 @@ def mainmenu():
             if choice == 3:
                 scode3(choice)
             if choice == 4:
-                scode4("", choice)
+                scode4(choice)
             if choice == 5:
                 scode5(choice)
             if choice == 6:
@@ -72,6 +72,7 @@ def mainmenu():
             if choice == 0:
                 i = 0
                 print("正在退出系统！！")
+                sys.exit(1)
         else:
             print("\033[1;31;40m    输入非法，请重新输入！！\033[0m")
             time.sleep(2)
@@ -132,6 +133,7 @@ def inputbox(showstr, showorder, length):
                     print("\033[1;31;40m必须输入" + str(length) + "个数字，请重新输入！！\033[0m")
                     return "0"
                 else:
+
                     return instr
             else:
                 print("\033[1;31;40m输入非法，请重新输入！！\033[0m")
@@ -160,6 +162,24 @@ def wfile(sstr, sfile, typeis, smsg, datapath):
         root.withdraw()
 
 
+def ffcode(scount, typestr, ismessage, schoice):
+    randstr.clear()
+    for j in range(int(scount)):
+        strpro = typestr[0].upper()
+        strtype = typestr[1].upper()
+        strclass = typestr[2].upper()
+        randfir = random.sample(number, 3)
+        randsec = sorted(randfir)
+        letterone = ""
+        for i in range(9):
+            letterone = letterone + random.choice(number)
+        sim = str(letterone[0:int(randsec[0])]) + strpro + str(letterone[int(randsec[0]):int(randsec[1])]) \
+              + strtype + str(letterone[int(randsec[1]):int(randsec[2])]) \
+              + strclass + str(letterone[int(randsec[2]):9]) + "\n"
+        randstr.append(sim)
+    wfile(randstr, typestr + "scode" + str(schoice) + ".txt", ismessage, "生成含数据分析功能的防伪码共计：", "codepath")
+
+
 def scode1(schoice):
     incount = inputbox("\033[1;32m    请输入您要生成的防伪码数量：\33[0m", 1, 0)
     while int(incount) == 0:
@@ -177,8 +197,8 @@ def scode1(schoice):
 def scode2(schoice):
     ordstart = inputbox("\033[1;32m    请输入系列产品起始号（3位）：\033[0m", 3, 3)
     while int(ordstart) == 0:
-        ordstart = inputbox("\033[1;32m    请输入系列产品起始号（3位）：\033[0m", 1, 0)
-    ordcount = inputbox("\033[1;32m    请输入系列产品数量：\033[0m", 1, 0)
+        ordstart = inputbox("\033[1;32m    请输入系列产品起始号（3位）：\033[0m", 1, 0)  # 起始号  如：123
+    ordcount = inputbox("\033[1;32m    请输入系列产品数量：\033[0m", 1, 0)  # 系列产品数量
     while int(ordcount) < 1 or int(ordcount) > 9999:
         ordcount = inputbox("\033[1;32m    请输入系列产品数量：\033[0m", 1, 0)
     incount = inputbox("\033[1;32m    请输入每个系列产品的防伪码数量：\033[0m", 1, 0)
@@ -195,15 +215,43 @@ def scode2(schoice):
 
 
 def scode3(schoice):
-    pass
+    # 每个系列产品的防伪码数量
+    incount = inputbox("\033[1;32m    请输入每个系列产品的防伪码数量：\033[0m", 1, 0)
+    while int(incount) == 0:
+        incount = inputbox("\033[1;32m    请输入每个系列产品的防伪码数量：\033[0m", 1, 0)
+    randstr.clear()
+    for j in range(int(incount)):
+        strone = ""  # 保存生成的单条防伪码，不带"-",循环时清空
+        for i in range(25):
+            strone = strone + random.choice(letter)
+        # 将生成的防伪码添加横线
+        strtwo = strone[:5] + "-" + strone[5:10] + "-" + strone[10:15] + "-" + strone[15:20] + "-" + strone[20:] + "\n"
+        randstr.append(strtwo)
+    wfile(randstr, "scode" + str(schoice) + ".txt", "", "已生成25位混合防伪序列码共计：", "codepath")
 
 
 def scode4(schoice):
-    pass
+    intype = inputbox("\033[1;32m    请输入数据分析编号（3位字母）：\033[0m", 2, 3)
+    while not str.isalpha(intype) or len(intype) != 3:
+        intype = inputbox("\033[1;32m    请输入数据分析编号（3位字母）：\033[0m", 2, 3)
+    incount = inputbox("\033[1;32m    请输入要生成的带数据分析功能的防伪码数量：\033[0m", 1, 0)
+    while int(incount) <= 0:
+        incount = inputbox("\033[1;32m    请输入要生成的带数据分析功能的防伪码数量：\033[0m", 1, 0)
+    ffcode(incount, intype, "", schoice)
 
 
 def scode5(schoice):
-    pass
+    default_dir = r"codeauto.mri"
+    file_path = tkinter.filedialog.askopenfilename(filetypes=[("Text file", "*.mri")], title=u"请选择智能批处理文件",
+                                                   initialdir=(os.path.expanduser(default_dir)))
+
+    codelist = openfile(file_path)
+    codelist = codelist.split("\n")
+    for item in codelist:
+        items = item.split(",")
+        codea = items[0]
+        codeb = items[1]
+        ffcode(codeb, codea, "no", schoice)
 
 
 def scode6(schoice):
@@ -211,11 +259,38 @@ def scode6(schoice):
 
 
 def scode7(schoice):
-    pass
+    mainid = inputbox("\033[1;32m    请输入EN13的国家代码（3位）：\033[0m", 1, 0)
+    while int(mainid) < 1 or len(mainid) != 3:
+        mainid = inputbox("\033[1;32m    请输入EN13的国家代码（3位）：\033[0m", 1, 0)
+    compid = inputbox("\033[1;32m    请输入EN13的企业代码（4位）：\033[0m", 1, 0)
+    while int(compid) < 1 or len(compid) != 4:
+        compid = inputbox("\033[1;32m    请输入EN13的企业代码（4位）：\033[0m", 1, 0)
+    incount = inputbox("\033[1;32m    请输入您要生成的条形码数量：\33[0m", 1, 0)
+    mkdir("barcode")
+    for j in range(int(incount)):
+        strone = ""
+        for i in range(5):
+            strone = strone + str(random.choice(number))
+        barcode = mainid + compid + strone
+        evensum = sum(range(1, 12, 2))
+        oddsum = sum(range(0, 12, 2))
+        checkbit = int((10 - (evensum * 3 + oddsum) % 10) % 10)
+        barcode = barcode + str(checkbit)
+        encoder = EAN13Encoder(barcode)
+        encoder.save("barcode\\" + barcode + ".png")
 
 
 def scode8(schoice):
-    pass
+    incount = inputbox("\033[1;32m    请输入您要生成的12位二维码数量：\33[0m", 1, 0)
+    while int(incount)==0:
+        incount = inputbox("\033[1;32m    请输入您要生成的12位二维码数量：\33[0m", 1, 0)
+    mkdir("qrcode")
+    for j in range(int(incount)):
+        strone = ""
+        for i in range(12):
+            strone = strone + str(random.choice(number))
+        encoder = qrcode.make(strone)
+        encoder.save("qrcode\\" + strone + ".png")
 
 
 def scode9(schoice):
